7dd6b6fd601eeb662745fd12814630eda24df7ec
Jacob Stuart <stuart4@purdue.edu>
2015-11-04 21:54:21 -0500

    changes definiton of problem so that the multiples are summed

--- main.ml
+++ main.ml
@@ -2,9 +2,8 @@ let sum_mults n =
         let sum = ref 0 in
         for i = 3 to n do
                 if (i mod 3) = 0 || (i mod 5) = 0 then
-                        sum := !sum + 1;
+                        sum := !sum + i;
         done;
         !sum;;
 
-let n = int_of_string Sys.argv.(1);;
-print_endline (string_of_int (sum_mults n));;
+print_endline (string_of_int (sum_mults 999));;
